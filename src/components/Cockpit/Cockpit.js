27077c73baf7728f182241dc17664f03672147b8
Maksim Kokuysky <m.kokuysky@corp.101xp.com>
2019-02-25 18:12:13 +0300

    started chapter 7 lecture 109

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,14 +1,19 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import classes from "./Cockpit.css";
+import AuthContext from '../../context/auth-context';
 
 const cockpit = (props) => {
 
+    const toggleBtnRef = useRef(null);
+
+    // executes after render()
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // HTTP request...
-        setTimeout(() => {
-            alert('Saved data to cloud!');
-        }, 1000);
+        // setTimeout(() => {
+        //     alert('Saved data to cloud!');
+        // }, 1000);
+        toggleBtnRef.current.click();
         return () => {
             console.log('[Cockpit.js] clean up work in useEffect');
         }
@@ -26,10 +31,10 @@ const cockpit = (props) => {
     if (props.showPersons) {
         btnClass = classes['Red'];
     }
-    if (props.persons.length <= 2) {
+    if (props.personsLength <= 2) {
         assignedClasses.push(classes['red']);
     }
-    if (props.persons.length <= 1) {
+    if (props.personsLength <= 1) {
         assignedClasses.push(classes['bold']);
     }
 
@@ -38,12 +43,16 @@ const cockpit = (props) => {
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button
+                ref={toggleBtnRef}
                 className={btnClass}
                 onClick={props.clicked}
             >
                 Toggle Persons
             </button>
+            <AuthContext.Consumer>
+                {(context) => <button onClick={context.login}>Log in</button>}
+            </AuthContext.Consumer>
         </div>
     );
 };
-export default cockpit;
+export default React.memo(cockpit);
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,48 @@
 import React, {Component} from 'react';
 import classes from './Person.css';
+import withClass from '../../../hoc/withClass';
+import Aux from '../../../hoc/Auxiliary';
+import PropTypes from 'prop-types';
+import AuthContext from '../../../context/auth-context';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef(); // new
+    }
+
+
+    componentDidMount() {
+        //this.inputElement.focus(); // old
+        this.inputElementRef.current.focus(); // new
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes['Person']}>
+            <Aux>
+                <AuthContext.Consumer>
+                    {(context) => context.authenticated ? <p>Authenticated!</p> : <p>Please log in!</p>}
+                </AuthContext.Consumer>
                 <p onClick={this.props.click}>I'm {this.props.name}, I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
+                <input
+                    type="text"
+                    //ref={(inputEl) => {this.inputElement = inputEl}} // old
+                    ref={this.inputElementRef} // new
+                    onChange={this.props.changed}
+                    value={this.props.name}
+                />
+            </Aux>
         )
     }
 }
 
-export default Person;
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
+
+export default withClass(Person, classes["Person"]);
