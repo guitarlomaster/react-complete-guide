52bd321f39cb669196bda1037cc54e1e20926e31
Maksim Kokuysky <m.kokuysky@corp.101xp.com>
2019-10-09 15:41:50 +0300

    End ch18 l355

---
+++ src/store/actions/auth.js
@@ -0,0 +1,42 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    }
+};
+
+export const authSuccess = (authData) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        authData: authData
+    }
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    }
+};
+
+export const auth = (email, password) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCo6o7Ezbjb6cJnmyOoh5BX35Jcr0NEo8g', authData)
+            .then((res => {
+                console.log(res);
+                dispatch(authSuccess(res.data));
+            }))
+            .catch((err) => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
+    }
+};
