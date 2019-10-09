8f8824bc6315fb817ca4718d3b6ff3e6db3e042b
Maksim Kokuysky <m.kokuysky@corp.101xp.com>
2019-10-09 19:45:28 +0300

    End ch18 l361

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 
@@ -38,7 +39,8 @@ class Auth extends Component {
                 valid: false,
                 touched: false,
             },
-        }
+        },
+        isSignup: true
     };
 
     checkValidity(value, rules) {
@@ -77,7 +79,13 @@ class Auth extends Component {
 
     submitHandler = (event) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+    };
+
+    switchAuthModeHandler = () => {
+        this.setState(prevState => {
+            return {isSignup: !prevState.isSignup}
+        });
     };
 
     render() {
@@ -88,7 +96,7 @@ class Auth extends Component {
                 config: this.state.controls[key]
             });
         }
-        const form = formElementsArray.map(formElement => (
+        let form = formElementsArray.map(formElement => (
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -100,21 +108,45 @@ class Auth extends Component {
                 changed={(event) => this.inputChangedHandler(event, formElement.id)}
             />
         ));
+
+        if (this.props.loading) {
+            form = <Spinner/>;
+        }
+
+        let errorMessage = null;
+
+        if (this.props.error) {
+            errorMessage = (
+                <p className={classes['Error']}>{this.props.error.message}</p>
+            )
+        }
         return (
             <div className={classes['Auth']}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
+                <Button
+                    btnType="Danger"
+                    clicked={this.switchAuthModeHandler}
+                >SWITCH TO {this.state.isSignup ? 'SIGNIN': 'SIGNUP'}</Button>
             </div>
         );
     }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapStateToProps = state => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        loading: state.auth.loading,
+        error: state.auth.error
     }
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,6 @@
 import * as actionTypes from './actionTypes';
 import axios from 'axios';
+import {AUTH_LOGOUT} from "./actionTypes";
 
 export const authStart = () => {
     return {
@@ -7,10 +8,11 @@ export const authStart = () => {
     }
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     }
 };
 
@@ -21,7 +23,21 @@ export const authFail = (error) => {
     }
 };
 
-export const auth = (email, password) => {
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
+
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -29,14 +45,41 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         };
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCo6o7Ezbjb6cJnmyOoh5BX35Jcr0NEo8g', authData)
-            .then((res => {
-                console.log(res);
-                dispatch(authSuccess(res.data));
-            }))
-            .catch((err) => {
-                console.log(err);
-                dispatch(authFail(err));
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCo6o7Ezbjb6cJnmyOoh5BX35Jcr0NEo8g';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCo6o7Ezbjb6cJnmyOoh5BX35Jcr0NEo8g';
+        }
+        axios.post(url, authData)
+            .then(res => {
+                //console.log(res);
+                dispatch(authSuccess(res.data.idToken, res.data.localId));
+                dispatch(checkAuthTimeout(res.data.expiresIn));
             })
+            .catch(err => {
+                const newErrObj = {
+                    ...err.response.data.error,
+                    message: getErrorMessageFromCode(err.response.data.error.message)
+                };
+                dispatch(authFail(newErrObj));
+            })
+    }
+};
+
+const getErrorMessageFromCode = (code) => {
+    switch (code) {
+        case 'EMAIL_EXISTS':
+            return 'The email address is already in use by another account.';
+        case 'OPERATION_NOT_ALLOWED':
+            return 'Password sign-in is disabled for this project.';
+        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+            return 'We have blocked all requests from this device due to unusual activity. Try again later.';
+        case 'EMAIL_NOT_FOUND':
+            return 'There is no user record corresponding to this identifier. The user may have been deleted.';
+        case 'INVALID_PASSWORD':
+            return 'The password is invalid or the user does not have a password.';
+        case 'USER_DISABLED':
+            return 'The user account has been disabled by an administrator.';
+        default:
+            return 'Something went wrong. Try again later.';
     }
 };
